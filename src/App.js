d7f49af41bfd43da8c044ae314fe444e148496cc
Vinicius de Alcantra Barros <viniciusalcantarabarros@gmail.com>
2021-07-27 14:22:01 -0300

    Footer montado

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css'
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header/Header';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false)
 
   useEffect(()=>{
     const loadAll = async() => {
@@ -25,8 +27,24 @@ export default () => {
     loadAll()
   }, [])
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10){
+        setBlackHeader(true)
+      }else{
+        setBlackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+    return() => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  })
+
   return (
     <div className="page">
+      <Header black={blackHeader} />
 
       {featuredData && 
         <FeaturedMovie item={featuredData} />
@@ -36,6 +54,13 @@ export default () => {
             <MovieRow key= {key} title={item.title} items={item.items} />
         ))}
       </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤️</span> por Vinícius de Alcântra<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos do site Themoviedb.org
+      </footer>
+
     </div>
   )
 }
